89809747a5539ae5e20c23093e4355ca280d7427
Rune Kaagaard <rumi.kg@gmail.com>
2020-06-11 14:43:03 +0200

    Beginning hashing of a view/schema.

--- testproj/testproj/movies/models.py
+++ testproj/testproj/movies/models.py
@@ -15,3 +15,10 @@ class Movie(m.Model):
     title = m.TextField("Movie Title")
     actors = m.ManyToManyField(Actor)
     director = m.ForeignKey(Director, on_delete=m.PROTECT)
+
+
+class ViewHash(m.Model):
+    version = m.PositiveIntegerField("The version")
+    schema_hash = m.TextField("The hash of the schema used in the view")
+    content_type_id = m.PositiveIntegerField("Content type id of object")
+    object_id = m.PositiveIntegerField("Id of object")
